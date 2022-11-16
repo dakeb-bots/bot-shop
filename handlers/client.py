61759efb369bf2997b6f4f65206e25e21de2cc3a
Egor Berdovskiy <dakebott@gmail.com>
2022-11-17 01:36:33 +0600

    Сделал хэндлер для кнопок

--- handlers/client.py
+++ handlers/client.py
@@ -1,8 +1,17 @@
-from create_bot import bot
+from create_bot import bot, dp
 from aiogram import Dispatcher, types
-
+from keyboards import markups
 async def welcome(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+    await bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! 🤖', reply_markup=markups.main_menu())
+
+@dp.callback_query_handler(lambda call: True)
+async def event_buttons(call: types.CallbackQuery):
+    if call.data == 'products':
+        await bot.send_message(call.from_user.id, 'Товары!')
+    elif call.data == 'about':
+        await bot.send_message(call.from_user.id, 'Информация!')
+    elif call.data == 'exit':
+        await bot.send_message(call.from_user.id, 'Выход!')
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(welcome)
