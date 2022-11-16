92172b3ccdeb7f8ad3b943a0f35570dad3bb8b7b
Egor Berdovskiy <dakebott@gmail.com>
2022-11-17 01:07:30 +0600

    Написал код к бд

---
+++ utils/sqlite_database.py
@@ -0,0 +1,18 @@
+import sqlite3 as sql
+from create_bot import bot
+
+def sql_start():
+    global base, cur
+    base = sql.connect('shop.db')
+    cur = base.cursor()
+    if base:
+        print('Database connected!')
+    # base.execute('CREATE TABLE IF NOT EXISTS item()')
+    base.commit()
+
+async def sql_query(query):
+    try:
+        cur.execute(query)
+    except Exception as ex:
+        print(ex)
+    base.commit()
